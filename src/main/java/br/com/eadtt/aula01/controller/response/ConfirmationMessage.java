d705105e52357bb23b6a2f35587dba6357b35426
Giovanni Silva <git@gsilva.pro>
2024-10-07 21:40:29 -0300

    Exemplo de ConfirmationMessage

---
+++ src/main/java/br/com/eadtt/aula01/controller/response/ConfirmationMessage.java
@@ -0,0 +1,18 @@
+package br.com.eadtt.aula01.controller.response;
+
+/**
+ * {
+ *     "status": "ERROR",
+ *     "confirmationMessages": [
+ *          {
+ *              "message": "Veiculo cadastrado no banco com sucesso",
+ *              "messageCode": "VEICULO_CADASTRADO"
+ *          }, {
+ *              "message": "Dono possui pendencias",
+ *              "CLIENTE_PAGAMENTO_PENDENTE"
+ *          }
+ *     ]
+ * }
+ */
+public class ConfirmationMessage {
+}
