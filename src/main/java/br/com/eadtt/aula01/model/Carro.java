9dfa68d4b3618178d230ccdb00f7e812cb3bebd7
Giovanni Silva <git@gsilva.pro>
2024-10-14 20:51:01 -0300

    persistencia de carro com spring jpa

--- src/main/java/br/com/eadtt/aula01/model/Carro.java
+++ src/main/java/br/com/eadtt/aula01/model/Carro.java
@@ -1,8 +1,6 @@
 package br.com.eadtt.aula01.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -12,8 +10,10 @@ import lombok.*;
 @EqualsAndHashCode(of = {"id"})
 @Entity
 @Table(name = "carro")
+@SequenceGenerator(name = "carro_seq", sequenceName = "carro_seq")
 public class Carro {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "carro_seq")
     private Integer id;
     private String marca;
     private String modelo;
--- src/main/java/br/com/eadtt/aula01/repository/CarroRepository.java
+++ src/main/java/br/com/eadtt/aula01/repository/CarroRepository.java
@@ -1,22 +1,35 @@
 package br.com.eadtt.aula01.repository;
 
 import br.com.eadtt.aula01.model.Carro;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class CarroRepository {
+    private final EntityManager em;
 
     public List<Carro> findAll() {
-        return null;
+        List<Carro> carros = em.createQuery("select c from Carro c", Carro.class)
+                .getResultList();
+        return carros;
     }
 
+    @Transactional
     public Carro save(Carro carro) {
-        return null;
+        if(carro.getId() == null) {
+            em.persist(carro);
+        } else {
+            em.merge(carro);
+        }
+        return carro;
     }
 
     public void deleteById(Integer id) {
-
+        em.remove(em.find(Carro.class, id));
     }
 }
