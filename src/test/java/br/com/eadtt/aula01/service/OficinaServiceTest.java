4b5fcee0bc4fe86a1fa6f5cc6883722d22487400
Giovanni Silva <git@gsilva.pro>
2024-11-11 20:58:19 -0300

    exemplo mockito

--- src/test/java/br/com/eadtt/aula01/service/OficinaServiceTest.java
+++ src/test/java/br/com/eadtt/aula01/service/OficinaServiceTest.java
@@ -1,9 +1,11 @@
 package br.com.eadtt.aula01.service;
 
 import br.com.eadtt.aula01.model.Oficina;
+import br.com.eadtt.aula01.model.exceptions.EntityNotFoundInDatabaseException;
 import br.com.eadtt.aula01.repository.EntradaCarroRepository;
 import br.com.eadtt.aula01.repository.OficinaRepository;
 import br.com.eadtt.aula01.repository.result.CarroProjection;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -12,8 +14,9 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 
 
 class OficinaServiceTest {
@@ -27,7 +30,7 @@ class OficinaServiceTest {
     void givenOficinaExistent_whenVeficarOcupacao_thenReturnCorrectCount() {
         OficinaService oficinaService = new OficinaService(oficinaRepository, entradaCarroRepository, entradaCarroService);
         Oficina oficina = new Oficina();
-        oficina.setOcupacaoMaxima(0);
+        oficina.setOcupacaoMaxima(2);
 
         doReturn(Optional.of(oficina))
                 .when(oficinaRepository)
@@ -38,10 +41,33 @@ class OficinaServiceTest {
         carros.add(carroProjection);
 
         doReturn(carros)
-                .when(entradaCarroRepository).getCarrosNaOficina(1);
+                .when(entradaCarroRepository).getCarrosNaOficina(anyInt());
 
         boolean result = oficinaService.verificarOcupacao(1);
 
         assertThat(result).isTrue();
+
+        verify(entradaCarroRepository, times(1)).getCarrosNaOficina(1);
+        verify(oficinaRepository, times(1)).findById(anyInt());
+    }
+
+    @Test
+    void givenOficinaNotExist_whenVeficarOcupacao_thenThrowException() {
+        // exemplo lançamento de exceção
+//        doThrow(new EntityNotFoundException("Not found"))
+//                .when(oficinaRepository)
+//                .findById(2);
+        doReturn(Optional.empty())
+                .when(oficinaRepository)
+                .findById(1);
+        OficinaService oficinaService = new OficinaService(oficinaRepository, entradaCarroRepository, entradaCarroService);
+
+        Object entityName = assertThatThrownBy(() -> {
+            oficinaService.verificarOcupacao(2);
+        }).isInstanceOf(EntityNotFoundInDatabaseException.class)
+                .hasMessage(null);
+//                .extracting("entityName");
+
+//        assertThat(entityName).isSameAs("Oficina");
     }
 }
